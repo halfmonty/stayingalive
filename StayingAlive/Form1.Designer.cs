1ae6c1f764cfa5f7a582fcf9360bc186ebe928ab
Mike Hubley <mikehubley@gmail.com>
2018-03-14 19:30:20 -0500

    second commit

---
+++ StayingAlive/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace StayingAlive
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackgroundImage = global::StayingAlive.Properties.Resources.waiting;
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(0, 0);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(278, 244);
+			this.button1.TabIndex = 0;
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(278, 244);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(300, 300);
+			this.MinimumSize = new System.Drawing.Size(300, 300);
+			this.Name = "Form1";
+			this.Text = "Staying Alive";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ StayingAlive/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Globalization;
+
+namespace StayingAlive
+{
+	public partial class Form1 : Form
+	{
+		BackgroundWorker m_oWorker;
+		WMPLib.WindowsMediaPlayer wplayer;
+		SpeechRecognitionEngine sre;
+		static bool startingUp = false;
+		static bool dieingDown = false;
+		System.IO.UnmanagedMemoryStream str;
+
+		public Form1()
+		{
+			InitializeComponent();
+			m_oWorker = new BackgroundWorker();
+			m_oWorker.DoWork += new DoWorkEventHandler(m_oWorker_DoWork);
+			m_oWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler
+					(m_oWorker_RunWorkerCompleted);
+			m_oWorker.WorkerSupportsCancellation = true;
+
+			wplayer = new WMPLib.WindowsMediaPlayer();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			byte[] b = StayingAlive.Properties.Resources.stayingalive1;
+			FileInfo fileInfo = new FileInfo("sa.mp3");
+			FileStream fs = fileInfo.OpenWrite();
+			fs.Write(b, 0, b.Length);
+			fs.Close();
+
+			wplayer.URL = fileInfo.Name;
+			wplayer.settings.setMode("loop", true);
+			wplayer.settings.volume = 0;
+
+			CultureInfo ci = new CultureInfo("en-us");
+			SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
+			sre.SetInputToDefaultAudioDevice();
+
+			Choices gb = new Choices();
+			gb.Add(new string[] { "staying alive", "staying", "alive", "say", "stay", "dive", "five", "playing", "thrive" });
+			GrammarBuilder grammarbuilder = new GrammarBuilder();
+			grammarbuilder.AppendDictation("grammar:dictation#pronunciation");
+			grammarbuilder.Append(gb);
+
+
+			Grammar gbb = new Grammar(gb);
+
+			sre.SetInputToDefaultAudioDevice();
+			sre.LoadGrammarAsync(gbb);
+			sre.SpeechRecognized += sre_SpeechRecognized;
+			sre.RecognizeAsync(RecognizeMode.Multiple);
+			
+		}
+
+		private void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+		{
+			if (e.Result.Text.ToLower().Contains("staying alive"))
+			{
+				if (!m_oWorker.IsBusy)
+				{
+					m_oWorker.RunWorkerAsync();
+					button1.BackgroundImage = StayingAlive.Properties.Resources.singing;
+					this.TopMost = true;
+					this.WindowState = FormWindowState.Normal;
+					this.Activate();
+					this.Focus();
+					Console.WriteLine("staying alive");
+				}
+			}
+		}
+
+		void m_oWorker_DoWork(object sender, DoWorkEventArgs e)
+		{
+			wplayer.controls.play();
+			startingUp = true;
+
+			for (int i = 0; i < 100; i++)
+			{
+				Thread.Sleep(100);
+                
+				//m_oWorker.ReportProgress(i);
+
+				if (m_oWorker.CancellationPending)
+				{
+					e.Cancel = true;
+					//wplayer.controls.pause();
+					if (startingUp) startingUp = false;
+					//return;
+				}
+
+				if (startingUp)
+				{
+					wplayer.settings.volume = wplayer.settings.volume + 2;
+					if (wplayer.settings.volume == 100) startingUp = false;
+				}else
+				{
+					wplayer.settings.volume = wplayer.settings.volume - 2;
+					if (wplayer.settings.volume == 0) return;
+				}
+			}
+		}
+
+		void m_oWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			wplayer.controls.pause();
+			button1.BackgroundImage = StayingAlive.Properties.Resources.waiting;
+			if (e.Cancelled)
+			{
+				//lblStatus.Text = "Task Cancelled.";
+			}
+
+			// Check to see if an error occurred in the background process.
+
+			else if (e.Error != null)
+			{
+				//lblStatus.Text = "Error while performing background operation.";
+			}
+			else
+			{
+				// Everything completed normally.
+				//lblStatus.Text = "Task Completed...";
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (m_oWorker.IsBusy)
+			{
+				m_oWorker.CancelAsync();
+				button1.BackgroundImage = StayingAlive.Properties.Resources.waiting;
+			}
+
+			this.TopMost = false;
+		}
+	}
+}
